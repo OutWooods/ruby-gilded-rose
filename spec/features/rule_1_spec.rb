8ea8bd1ef36afe6b0be23e82fd6ee31bc1aa1b2d
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 13:01:02 +0000

    test off food goes down by twice as much

--- spec/features/rule_1_spec.rb
+++ spec/features/rule_1_spec.rb
@@ -3,15 +3,17 @@ require './src/item'
 
 describe 'lowers quality by one on a normal object' do
   subject(:rocket) { Item.new('Rocket', 10, 10) }
-  subject(:rose) { GildedRose.new([rocket])}
+  subject(:off_rocket) { Item.new('Rocket', 0, 10) }
+  subject(:rose) { GildedRose.new([rocket, off_rocket])}
 
   describe '#update_quality' do
     it 'should reduce normal foods quality within sell_in by default amount' do
-      expect { rose.update_quality }.to change { rocket.quality }.by -1
+      expect { rose.update_quality }.to change { rocket.quality }.by -GildedRose::NORMAL_DEGRADATION
     end
 
-    # it 'should reduce food within sell_in by one for both' do
-    #   expect { rose.update_quality }.to change { rocket.quality }.by -2
-    # end
+    it 'should reduce food outside sell_in by twice the normal amount' do
+      quality_reduction = GildedRose::NORMAL_DEGRADATION * -2
+      expect { rose.update_quality }.to change { off_rocket.quality }.by quality_reduction
+    end
   end
 end
