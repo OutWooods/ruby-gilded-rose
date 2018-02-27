ee31ea5b5a6f0f83a7e153fe87de806f2d8f9a8f
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 11:51:16 +0000

    test item stores sell_in

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,9 +3,19 @@ require './src/item'
 describe Item do
 
   describe '#initialize' do
+    it 'should store a name' do
+      item = Item.new('Food', 'Another', 'Thing')
+      expect(item.name).to eq 'Food'
+    end
+
     it 'should store a food' do
       item = Item.new('Food', 'Another', 'Thing')
       expect(item.name).to eq 'Food'
+    end
+
+    it 'should store a sell_in' do
+      item = Item.new('Food', 'When', 'Thing')
+      expect(item.sell_in).to eq 'When'
     end
   end
 
