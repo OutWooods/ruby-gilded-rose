e02bbf647f426383191dfa4375f3524ef6c8c289
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 14:28:02 +0000

    refactor test suite
    mainly tidying up formatting and indentation in sell in change

--- spec/features/rule_1_spec.rb
+++ spec/features/rule_1_spec.rb
@@ -6,19 +6,16 @@ describe 'lowers quality by one on a normal object' do
   subject(:off_rocket) { Item.new('Rocket', 0, 10) }
   subject(:rose) { GildedRose.new([rocket, off_rocket]) }
 
-  describe '#update_quality' do
-    describe '#quality' do
-      it 'should reduce normal foods quality within sell_in by default amount' do
-        single_rose = GildedRose.new([rocket])
-        expect { single_rose.update_quality }.to change { rocket.quality }.by(-1)
-      end
+  describe '#quality' do
+    it 'should reduce normal foods quality within sell_in by default amount' do
+      single_rose = GildedRose.new([rocket])
+      expect { single_rose.update_quality }.to change { rocket.quality }.by(-1)
+    end
 
-      it 'should reduce food outside sell_in by twice the normal amount' do
-        single_rose = GildedRose.new([off_rocket])
-        change = -2
-        expect { single_rose.update_quality }.to change { off_rocket.quality }.by change
-      end
-
+    it 'should reduce food outside sell_in by twice the normal amount' do
+      single_rose = GildedRose.new([off_rocket])
+      change = -2
+      expect { single_rose.update_quality }.to change { off_rocket.quality }.by change
     end
   end
 end
--- spec/features/sell_in_change_spec.rb
+++ spec/features/sell_in_change_spec.rb
@@ -5,18 +5,17 @@ describe 'changes to sell in' do
   subject(:rocket) { Item.new('Rocket', 10, 10) }
   subject(:off_rocket) { Item.new('Rocket', 0, 10) }
   subject(:brie) { Item.new('Aged Brie', 0, 10) }
-  subject(:sulfras) { Item.new("Sulfuras, Hand of Ragnaros", 1, 10) }
-  subject(:rose) { GildedRose.new([rocket, off_rocket]) }
+  subject(:sulfras) { Item.new('Sulfuras, Hand of Ragnaros', 1, 10) }
 
   describe '#sell_in' do
     it 'should reduce normal foods by default amount' do
       single_rose = GildedRose.new([rocket])
-      expect { rose.update_quality }.to change { rocket.sell_in }.by(-1)
+      expect { single_rose.update_quality }.to change { rocket.sell_in }.by(-1)
     end
 
     it 'should reduce off foods by default amount' do
       single_rose = GildedRose.new([off_rocket])
-      expect { rose.update_quality }.to change { off_rocket.sell_in }.by(-1)
+      expect { single_rose.update_quality }.to change { off_rocket.sell_in }.by(-1)
     end
 
     it 'should reduce aged brie\'s sell in by the normal amount' do
@@ -28,6 +27,5 @@ describe 'changes to sell in' do
       single_rose = GildedRose.new([sulfras])
       expect { single_rose.update_quality }.not_to change { sulfras.sell_in }
     end
-
   end
 end
