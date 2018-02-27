8e51d5223c8d0d37ece783b0144d9c1f3243fb82
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 11:49:38 +0000

    test item has a name

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,12 @@
 require './src/item'
+
+describe Item do
+
+  describe '#initialize' do
+    it 'should store a food' do
+      item = Item.new('Food', 'Another', 'Thing')
+      expect(item.name).to eq 'Food'
+    end
+  end
+
+end
