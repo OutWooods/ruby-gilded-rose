4a997c6e97bc2ca43538524075fe8ad1beee5758
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 11:57:38 +0000

    note issue with test, but as its legacy leave in for now

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,9 +2,13 @@ require './src/gilded_rose'
 
 describe GildedRose do
 
+
+
   describe "#update_quality" do
+    # uncertain about the purpose of this test.
+    #Remove later as its not testing much. 
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
+      items = double('')
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
