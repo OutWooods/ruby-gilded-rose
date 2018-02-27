2265b81bd5bea85fc5feae45d1db519a8becc9fb
Tagrand <thomas.a.grand@gmail.com>
2018-02-27 14:34:21 +0000

    test normal food can\'t go below 0
    checks food on quality of 0, in sell_in does not decrease in quality further

---
+++ spec/features/rule_2_spec.rb
@@ -0,0 +1,16 @@
+require './src/gilded_rose'
+require './src/item'
+
+describe 'lowers quality by one on a normal object' do
+  subject(:low_quality_rocket) { Item.new('Rocket', 10, 0) }
+  subject(:low_quality_off_rocket) { Item.new('Rocket', 0, 1) }
+  subject(:rose) { GildedRose.new([rocket, off_rocket]) }
+
+  describe '#quality' do
+    it 'should not reduce a normal foods quality below 0' do
+      single_rose = GildedRose.new([low_quality_rocket])
+      single_rose.update_quality
+      expect(low_quality_rocket.quality).to be >= 0
+    end
+  end
+end
